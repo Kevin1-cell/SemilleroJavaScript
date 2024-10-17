774b0f9bedee5223d925fb72716a20aaac3943e3
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2024-10-17 13:43:04 -0500

    Exercise 4 from operator practice hierarchy done

---
+++ CLASE1_VARIABLES SEMANA1/Practica de Operadores/Geranquia de operadores/problema4/problema4.js
@@ -0,0 +1,14 @@
+/*
+Ejercicio
+Resuelva la siguiente expresión paso a paso:
+4) -1*(15/5-2)+2-4%4+2
+
+--Solución
+-1 * (15 / 5 - 2) + 2 - 4 % 4 + 2;
+-1 * (3 - 2) + 2 - 0 + 2;
+-1 * 1 + 4;
+= 3;
+*/
+
+let resultado =-1*(15/5-2)+2-4%4+2;
+console.log("El resultado es: ",resultado);
