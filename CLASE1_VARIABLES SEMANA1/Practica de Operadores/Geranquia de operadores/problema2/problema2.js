2df1581de4a5fc63c29d2b6856c6109c177a5b81
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2024-10-17 13:31:06 -0500

    Exercise 2 from operator practice hierarchy done

--- CLASE1_VARIABLES SEMANA1/Practica de Operadores/Geranquia de operadores/problema2/problema2.js
+++ CLASE1_VARIABLES SEMANA1/Practica de Operadores/Geranquia de operadores/problema2/problema2.js
@@ -1,14 +1,14 @@
 /*
 Ejercicio
 Resuelva la siguiente expresión paso a paso:
-1) 10%2*5/10+3*(5+2*3)%60
+2) -3*(4-8)+15-3*15-0%5
 
 --Solución
-10 % 2 * 5 / 10 + 3 * (5 + 2 * 3) % 60
-10 % 10 / 10 + 3 * (5 + 6) % 60
-0 / 10 + 33 % 60
-= 33
+-3 * (4 - 8) + 15 - 3 * 15 - 0 % 5
+-3 * -4 + 15 - 45 - 0
+12 + 15 - 45
+= -18
 */
 
-let resultado = 10%2*5/10+3*(5+2*3)%60;
+let resultado = -3*(4-8)+15-3*15-0%5;
 console.log("El resultado es: ",resultado);
