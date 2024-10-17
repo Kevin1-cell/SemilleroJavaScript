44a65bd483874edb8868db94338f8709efaeec68
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2024-10-17 14:15:59 -0500

    Exercise 8 from operator practice hierarchy done

---
+++ CLASE1_VARIABLES SEMANA1/Practica de Operadores/Geranquia de operadores/problema8/problema8.js
@@ -0,0 +1,17 @@
+/*
+Ejercicio
+Resuelva la siguiente expresión paso a paso:
+8) 5%1+2%2*12565-7*(4/2/2*8-8)
+
+--Solución
+5 % 1 + 2 % 2 * 12565 - 7 * (4 / 2 / 2 * 8 - 8);
+0 + 0 * 12565 - 7 * (2 / 2 * 8 - 8);
+0 + 0 * 12565 - 7 * (1 * 8 - 8); 
+0 + 0 * 12565 - 7 * (0); 
+0 * 12565 - 0; 
+0 - 0
+= 0
+*/
+
+let resultado = 5%1+2%2*12565-7*(4/2/2*8-8);
+console.log("El resultado es: ",resultado);
