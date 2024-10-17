a559fbb7ebbe0d52e97acb699189ea95ae969968
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2024-10-17 14:21:33 -0500

    Exercise 9 from operator practice hierarchy done

---
+++ CLASE1_VARIABLES SEMANA1/Practica de Operadores/Geranquia de operadores/problema9/problema9.js
@@ -0,0 +1,15 @@
+/*
+Ejercicio
+Resuelva la siguiente expresión paso a paso:
+9) (5/3)+(((48%15)-1)/3)+10/5*2
+
+--Solución
+(5 / 3) + (((48 % 15) - 1) / 3) + 10 / 5 * 2;
+1.66 + ((3 - 1) / 3) + 2 * 2;
+1.66 + (2 / 3) + 4;
+1.66 + 0.66 + 4;
+= 6.32
+*/
+
+let resultado = (5/3)+(((48%15)-1)/3)+10/5*2;
+console.log("El resultado es: ",resultado);
