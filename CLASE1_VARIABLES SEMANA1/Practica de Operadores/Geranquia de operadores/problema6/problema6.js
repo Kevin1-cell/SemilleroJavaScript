bc0d022f9dbd3f269e2cbd7985787e395fb05e5d
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2024-10-17 14:00:40 -0500

    Exercise 6 from operator practice hierarchy done

---
+++ CLASE1_VARIABLES SEMANA1/Practica de Operadores/Geranquia de operadores/problema6/problema6.js
@@ -0,0 +1,15 @@
+/*
+Ejercicio
+Resuelva la siguiente expresión paso a paso:
+6) 3*50/5+90%100-100/(4*-3+2)
+
+--Solución
+3 * 50 / 5 + 90 % 100 - 100 / (4 * - 3 + 2);
+150 / 5 + 90 - 100 /(-12 + 2);
+30 + 90 - 100 / -10
+30 + 90 + 10
+=130
+*/
+
+let resultado =3*50/5+90%100-100/(4*-3+2);
+console.log("El resultado es: ",resultado);
