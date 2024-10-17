48a6a41031c6b46cde3cbc9af4314d76fb649abb
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2024-10-17 14:27:04 -0500

    Exercise 10 from operator practice hierarchy done

---
+++ CLASE1_VARIABLES SEMANA1/Practica de Operadores/Geranquia de operadores/problema10/problema10.js
@@ -0,0 +1,14 @@
+/*
+Ejercicio
+Resuelva la siguiente expresión paso a paso:
+10) ((-5*12)/5)-6%2+3
+
+--Solución
+((-5 * 12) / 5) - 6 % 2 + 3;
+(-60 / 5) - 0 + 3;
+-12 + 3;
+=-9;
+*/
+
+let resultado = ((-5*12)/5)-6%2+3;
+console.log("El resultado es: ",resultado);
