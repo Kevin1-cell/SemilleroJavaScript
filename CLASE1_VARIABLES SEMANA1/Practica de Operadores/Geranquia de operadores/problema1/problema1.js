fb7258780d86c59882a8a6a4365c25bcbb519b4e
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2024-10-17 13:22:39 -0500

    New folder added in operator practice

---
+++ CLASE1_VARIABLES SEMANA1/Practica de Operadores/Geranquia de operadores/problema1/problema1.js
@@ -0,0 +1,11 @@
+/*
+Ejercicio
+Resuelva la siguiente expresión paso a paso:
+1) 10%2*5/10+3*(5+2*3)%60
+
+--Solución
+
+*/
+
+let resultado = 10%2*5/10+3*(5+2*3)%60;
+console.log("El resultado es: ",resultado);
