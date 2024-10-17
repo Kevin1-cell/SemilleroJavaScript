ac43a91a670c8d466f22a7c9738c4548103d86a4
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2024-10-17 13:36:46 -0500

    Exercise 3 from operator practice hierarchy done

---
+++ CLASE1_VARIABLES SEMANA1/Practica de Operadores/Geranquia de operadores/problema3/problema3.js
@@ -0,0 +1,16 @@
+/*
+Ejercicio
+Resuelva la siguiente expresión paso a paso:
+3) -15%3-15000/3000*(4*(-12+15*6))/10
+
+--Solución
+-15 % 3 - 15000 / 3000 * (4 * (-12 + 15 * 6)) / 10
+0 - 5 * (4 * (-12 + 90)) / 10
+-5 * (4 * 78) / 10
+-5 * 312 / 10
+-1.560 / 10
+-156
+*/
+
+let resultado = -15%3-15000/3000*(4*(-12+15*6))/10;
+console.log("El resultado es: ",resultado);
