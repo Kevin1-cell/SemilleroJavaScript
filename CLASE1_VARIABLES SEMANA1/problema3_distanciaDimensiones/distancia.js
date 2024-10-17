7b8910d3d5ead0bc2bb1c3defab53cf5902c2081
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2024-10-17 10:44:30 -0500

    Algorithms one exercise added

---
+++ CLASE1_VARIABLES SEMANA1/problema3_distanciaDimensiones/distancia.js
@@ -0,0 +1,9 @@
+//3. Realice un programa que calcule la distancia entre dos puntos en el espacio
+//tridimensional.
+
+
+let punto1 = parseInt(prompt("Ingrese el punto uno"));
+let punto2 = parseInt(prompt("Ingrese el punto dos"));
+
+let distancia = punto2 - punto1;
+console.log(`La distancia entre el punto ${punto1} y el punt ${punto2} es ${distancia}`);
---
+++ CLASE1_VARIABLES SEMANA1/problema4_algoritmos1_url/url.js
@@ -0,0 +1,15 @@
+/*
+Cree un programa que declare e
+inicialice una constante que
+almacene el número pi y otra que
+almacena la URL de Google. Imprima
+cada una de las variables. Haga
+código.
+*/
+
+const pi = 3.1415;
+console.log("El numero pi equivale a ",pi);
+
+let URL_GOOGLE = "https://th.bing.com/th/id/R.9b238e4a5a46f34b4cc311bcebae901f?rik=RSs2mU86QbP08Q&pid=ImgRaw&r=0";
+console.log("La url de google es: ",URL_GOOGLE);
+ 
