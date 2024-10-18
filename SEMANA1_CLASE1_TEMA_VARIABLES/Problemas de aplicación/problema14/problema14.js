093b911373eb07d770f8295334442b9c59d6965b
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2024-10-18 11:10:52 -0500

    Mathematical operators exercise 14 workshop 1

---
+++ SEMANA1_CLASE1_TEMA_VARIABLES/Problemas de aplicación/problema14/problema14.js
@@ -0,0 +1,11 @@
+/*
+14. Cree un programa que genere un número aleatorio dentro de un rango específico.
+*/
+
+let numeroAleatorio = Math.random()*10;
+
+alert("El numero generado con decimales hasta 10 es "+numeroAleatorio.toFixed(2));
+
+let numeroAleatorio2 = Math.floor(Math.random()*10);
+
+alert("El numero generado con decimales hasta 10 es "+numeroAleatorio2);
