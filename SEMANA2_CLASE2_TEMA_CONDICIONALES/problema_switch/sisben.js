298aff795561c4cdec841869417116915072ef07
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2024-10-21 16:32:03 -0500

    New sisten exercise done

--- SEMANA2_CLASE2_TEMA_CONDICIONALES/problema_switch/sisben.js
+++ SEMANA2_CLASE2_TEMA_CONDICIONALES/problema_switch/sisben.js
@@ -1,4 +1,26 @@
 /*
-Un programa que muestre el sistem de un usuario y que muestre si tiene derecho o no. El usuario
+Un programa que muestre el sistema de un usuario y que muestre si tiene derecho o no. El usuario
 tiene derecho si el sisbén es menor que tres
 */
+
+
+let numero = parseInt(prompt("Ingrese su numero de sisbén"));
+
+switch(numero){
+    case 1:
+        console.log("No tiene derecho");
+        break;
+    case 2:
+        console.log("No tiene derecho");
+        break;
+    case 3:
+        console.log("Tiene derecho, SIuuuuu");
+        break;
+    default:
+        console.log("Ingrese alguna opción valida");
+        break;
+}
+
+       
+       
+    
