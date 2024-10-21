feb513552c1ffd9f4bd127c06463cea4911723b8
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2024-10-21 17:33:21 -0500

    First exercise of the guide, point b

--- SEMANA2_CLASE2_TEMA_CONDICIONALES/Taller1_guiaSemana2Principal/problema1/taller.js
+++ SEMANA2_CLASE2_TEMA_CONDICIONALES/Taller1_guiaSemana2Principal/problema1/taller.js
@@ -5,6 +5,7 @@ b) Recorra la cadena usando ciclo for
 */
 
 alert("“Javascript es un buen lenguaje”");
+alert("Punto a");
 let palabra = "Javascript es un buen lenguaje";
 
 console.log("Posición 3: "+palabra[3]);
@@ -12,3 +13,8 @@ console.log("Posición 6: "+palabra[6]);
 console.log("Posición 8: "+palabra[8]);
 
 
+alert("Punto b");
+
+for(let i = 0; i < palabra.length; i++){
+    console.log(palabra[i]);
+}
