a28823bb2e1865b68d70b6663b0bbccf9f708f88
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2024-10-21 17:28:29 -0500

    First exercise of the guide

---
+++ SEMANA2_CLASE2_TEMA_CONDICIONALES/Taller1_guiaSemana2Principal/problema1/taller.js
@@ -0,0 +1,14 @@
+/*
+1.Dada la cadena “Javascript es un buen lenguaje”:
+a) Leer e imprimir los elementos de índices 3, 6, 8
+b) Recorra la cadena usando ciclo for
+*/
+
+alert("“Javascript es un buen lenguaje”");
+let palabra = "Javascript es un buen lenguaje";
+
+console.log("Posición 3: "+palabra[3]);
+console.log("Posición 6: "+palabra[6]);
+console.log("Posición 8: "+palabra[8]);
+
+
